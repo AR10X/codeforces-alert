50349883129581892d021c98fb381f59dd08fdfe
AR10X <arehamanofficial@gmail.com>
2023-01-26 22:32:11 +0530

    Ejection of webpack

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import connectDB from './db'
+import App from './client/src/App';
+import connectDB from './server/config/db';
+const publicUrl = process.env.PUBLIC_URL;
+
 connectDB();
 
 
